afffd03024ac5d023066f164ca94c9cd1c7b9895
luciamsotelo <142464028+luciamsotelo@users.noreply.github.com>
2023-10-15 21:53:16 -0700

    Update index.js
    assignment

--- index.js
+++ index.js
@@ -66,13 +66,13 @@ function parseLocations(results){
 // }
 
 // Metro station markers
-const rS = L.marker([48.866200610611926, 2.352236247419453], {icon: redIcon}).bindPopup('RÃ©aumur-SÃ©bastopol')
-const sSD = L.marker([48.869531786321566, 2.3528590208055196]).bindPopup('Strasbourg-Saint-Denis')
-const sentier = L.marker([48.8673721067762, 2.347107922912739]).bindPopup('Sentier')
-const bourse = L.marker([48.86868503971672, 2.3412285142058167]).bindPopup('Bourse')
-const qS = L.marker([48.869560129483226, 2.3358638645569543]).bindPopup('Quatre Septembre')
-const gB = L.marker([48.871282159004856, 2.3434818588892714]).bindPopup('Grands Boulevards')
+// const rS = L.marker([48.866200610611926, 2.352236247419453], {icon: redIcon}).bindPopup('RÃ©aumur-SÃ©bastopol')
+// const sSD = L.marker([48.869531786321566, 2.3528590208055196]).bindPopup('Strasbourg-Saint-Denis')
+// const sentier = L.marker([48.8673721067762, 2.347107922912739]).bindPopup('Sentier')
+// const bourse = L.marker([48.86868503971672, 2.3412285142058167]).bindPopup('Bourse')
+// const qS = L.marker([48.869560129483226, 2.3358638645569543]).bindPopup('Quatre Septembre')
+// const gB = L.marker([48.871282159004856, 2.3434818588892714]).bindPopup('Grands Boulevards')
 
-const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap)
+// const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap)
 
 
